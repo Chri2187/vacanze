0ceeacfad863e6d6f6ae692a109644face1a79ee
Christian Francini Mascagni <christian.francini87@gmail.com>
2022-03-02 23:46:56 +0100

    Major Update
    Creazione componenti e funzioni per fetch dati e render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Title from './components/Title';
+import Holiday from './components/Holiday';
+//Ho utilizzato Cartella Component per dare una chiara locazione ai miei componenti
 function App() {
-  return <div className='App'></div>;
+  return (
+    <section className='section-center'>
+      <div className='container'>
+        <Title text='Le Nostre Vacanze'/>
+        <Holiday />
+      </div>
+    </section>
+  );
 }
 
 export default App;
---
+++ src/components/Holiday.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import SingleHoliday from './SingleHoliday';
+const url = 'https://react-corso-api.netlify.app/.netlify/functions/holiday';
+
+const Holiday = () => {
+  const [data, setData] = useState([]);
+  const [selected, setSelected] = useState(0);
+
+  const nextHoliday = () => {
+    setSelected((prevValue) => {
+      if (prevValue + 1 === data.data.length) {
+        return 0;
+      } else {
+        return prevValue + 1;
+      }
+    });
+  };
+  const prevHoliday = () => {
+    setSelected((prevValue) => {
+      if (prevValue - 1 < 0) {
+        return data.data.length - 1;
+      } else {
+        return prevValue - 1;
+      }
+    });
+  };
+  // fetch
+  const getData = async () => {
+    try {
+      const response = await axios.get(url);
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  // Conditional return
+  if (data.success === true) {
+    return (
+      <>
+        {data.data.length > 0 ? (
+          <SingleHoliday
+            {...data.data[selected]}
+            next={nextHoliday}
+            prev={prevHoliday}
+          />
+        ) : (
+          <h4>No Vacanze</h4>
+        )}
+      </>
+    );
+  } else {
+    return <h2>...Loading</h2>;
+  }
+};
+
+export default Holiday;
